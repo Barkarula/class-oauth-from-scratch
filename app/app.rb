d5ddb8dbe15a628012bb2b6185b73e2a1cb10a1f
Chris Mather <chris@eventedmind.com>
2016-03-23 11:19:45 -0700

    added session login / logout

--- app/app.rb
+++ app/app.rb
@@ -5,5 +5,31 @@ require "securerandom"
 require "json"
 require "active_support/all"
 
+Tilt.register Tilt::ERBTemplate, 'html.erb'
+
+# configure cookie sessions
+set :sessions,
+  key: "_app_session",
+  path: "/",
+  secret: "super secret",
+  expire_after: 365.days
+
+helpers do
+  def logged_in?
+    session[:logged_in]
+  end
+end
+
 get "/" do
+  erb :home
+end
+
+get "/login" do
+  session[:logged_in] = true
+  [302, {"Location" => "/"}, []]
+end
+
+get "/logout" do
+  session[:logged_in] = false
+  [302, {"Location" => "/"}, []]
 end
